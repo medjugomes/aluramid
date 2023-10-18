68512d06ecada952cca229b655db612c090a2769
medjugomes <136620326+medjugomes@users.noreply.github.com>
2023-10-18 12:36:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,14 @@ document.querySelectorAll('.tecla_pom')
 listaDeTeclas[0].onclick = tocaSomPom;
 
 while(contador < 9){
-listadeteclas[0].onclick = tocaSomPom;
+    const instrumento = listaDeTeclas[contador].onclick = function(){
+const instrumento = listaDeTeclas[contador].classList
+}
 
-};
+}
+const instrumento = listaDeTeclas[contador].onclick = function(){
+
+}
+
+
 
