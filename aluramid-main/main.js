9268a63bc03ce5b0bec9f7a66a5ac81c27bc3049
medjugomes <136620326+medjugomes@users.noreply.github.com>
2023-11-01 08:16:17 -0300

    Add files via upload

---
+++ aluramid-main/main.js
@@ -0,0 +1,21 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play('');
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+while(contador < 9){
+    const instrumento = listaDeTeclas[contador].onclick = function(){
+const instrumento = listaDeTeclas[contador].classList
+}
+
+}
+const instrumento = listaDeTeclas[contador].onclick = function(){
+
+}
+
+
+
+s
